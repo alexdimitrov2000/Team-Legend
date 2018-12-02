a272aedc803c9eec93ee452ef0177e079b0a97bd
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2018-12-02 22:59:01 +0200

    Scaffolded four identity pages.

---
+++ TeamLegend/TeamLegend.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TeamLegend.Data;
+using TeamLegend.Models;
+
+[assembly: HostingStartup(typeof(TeamLegend.Web.Areas.Identity.IdentityHostingStartup))]
+namespace TeamLegend.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

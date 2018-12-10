2fec16087ae926f0a144bbaeecb72d16914c44f9
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2018-12-11 00:00:22 +0200

    Added more constants for the other models' pictures.

--- TeamLegend/TeamLegend.Common/GlobalConstants.cs
+++ TeamLegend/TeamLegend.Common/GlobalConstants.cs
@@ -2,6 +2,12 @@
 {
     public static class GlobalConstants
     {
-        public static string ProfilePicture = "{0}_profilePicture";
+        public static string ProfilePicture = "{0}_profilePicture.jpg";
+
+        public static string PlayerPicture = "{0}_playerPicture";
+
+        public static string StadiumPicture = "{0}_stadiumPicture";
+
+        public static string BadgePicture = "{0}_badge";
     }
 }

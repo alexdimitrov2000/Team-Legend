ba5eda1d73a74d599fd7e83a7e1042118f40cf57
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2018-12-16 01:48:45 +0200

    Fixed displaying the PlayingPosition enum values.

--- TeamLegend/TeamLegend.Models/Enums/PlayingPosition.cs
+++ TeamLegend/TeamLegend.Models/Enums/PlayingPosition.cs
@@ -1,18 +1,19 @@
 ﻿namespace TeamLegend.Models.Enums
 {
     using System.ComponentModel;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public enum PlayingPosition
     {
-        [DisplayName("Goalkeeper")] Goalkeeper = 1,
-        [DisplayName("Center Back Defender")] CenterBackDefender = 2,
-        [DisplayName("Left Back Defender")] LeftBackDefender = 3,
-        [DisplayName("Right Back Defender")] RightBackDefender = 4,
-        [DisplayName("Center Midfielder")] CenterMidfielder = 5,
-        [DisplayName("Left Midfielder")] LeftMidfielder = 6,
-        [DisplayName("Right Midfielder")] RightMidfielder = 7,
-        [DisplayName("Center Forward")] CenterForward = 8,
-        [DisplayName("Left Wing Forward")] LeftWingForward = 9,
-        [DisplayName("Right Wing Forward")] RightWingForward = 10,
+        [Display(Name = "Goalkeeper")] Goalkeeper = 1,
+        [Display(Name = "Center Back Defender")] CenterBackDefender = 2,
+        [Display(Name = "Left Back Defender")] LeftBackDefender = 3,
+        [Display(Name = "Right Back Defender")] RightBackDefender = 4,
+        [Display(Name = "Center Midfielder")] CenterMidfielder = 5,
+        [Display(Name = "Left Midfielder")] LeftMidfielder = 6,
+        [Display(Name = "Right Midfielder")] RightMidfielder = 7,
+        [Display(Name = "Center Forward")] CenterForward = 8,
+        [Display(Name = "Left Wing Forward")] LeftWingForward = 9,
+        [Display(Name = "Right Wing Forward")] RightWingForward = 10,
     }
 }

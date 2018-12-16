e8b94f6615f7d42163c5d45f5ababc1ac430ba6c
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2018-12-16 04:09:30 +0200

    Introduced constants for the folders where pictures are being stored. Refactored the methods and implemented team badge uploading and requesting url.

--- TeamLegend/TeamLegend.Services/CloudinaryService.cs
+++ TeamLegend/TeamLegend.Services/CloudinaryService.cs
@@ -1,14 +1,20 @@
 ﻿namespace TeamLegend.Services
 {
-    using CloudinaryDotNet;
-    using CloudinaryDotNet.Actions;
     using Common;
     using Contracts;
+
+    using CloudinaryDotNet;
+    using CloudinaryDotNet.Actions;
+
     using System.IO;
-    using Infrastructure.Extensions;
 
     public class CloudinaryService : ICloudinaryService
     {
+        private const string TeamBadgesFolder = "TeamBadges";
+        private const string PlayerPicturesFolder = "PlayerPictures";
+        private const string ProfilePicturesFolder = "ProfilePictures";
+        private const string StadiumPicturesFolder = "StadiumPictures";
+
         private readonly Cloudinary cloudinary;
 
         public CloudinaryService()
@@ -25,7 +31,7 @@
             ImageUploadParams imageUploadParams = new ImageUploadParams
             {
                 File = new FileDescription(profilePictureId, fileStream),
-                Folder = "ProfilePictures",
+                Folder = ProfilePicturesFolder,
                 PublicId = profilePictureId
             };
 
@@ -38,7 +44,7 @@
             ImageUploadParams imageUploadParams = new ImageUploadParams
             {
                 File = new FileDescription(stadiumPictureId, fileStream),
-                Folder = "StadiumPictures",
+                Folder = StadiumPicturesFolder,
                 PublicId = stadiumPictureId
             };
 
@@ -50,8 +56,20 @@
             ImageUploadParams imageUploadParams = new ImageUploadParams
             {
                 File = new FileDescription(playerPictureId, fileStream),
-                Folder = "PlayerPictures",
+                Folder = PlayerPicturesFolder,
                 PublicId = playerPictureId
+            };
+
+            return this.cloudinary.Upload(imageUploadParams);
+        }
+
+        public ImageUploadResult UploadTeamBadgePicture(string badgeId, Stream fileStream)
+        {
+            ImageUploadParams imageUploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(badgeId, fileStream),
+                Folder = TeamBadgesFolder,
+                PublicId = badgeId
             };
 
             return this.cloudinary.Upload(imageUploadParams);
@@ -61,8 +79,8 @@
         {
             if (username == null || imageVersion == null)
                 return null;
-
-            string path = "/ProfilePictures/" + string.Format(GlobalConstants.ProfilePicture, username);
+            
+            string path = string.Format(GlobalConstants.FilePath, ProfilePicturesFolder, string.Format(GlobalConstants.ProfilePicture, username));
             var pictureUrl = cloudinary.Api.UrlImgUp.Transform(new Transformation().Gravity("face").Width(30).Height(30).Zoom(0.7).Crop("thumb"))
                                     .Version(imageVersion).BuildUrl(path);
             return pictureUrl;
@@ -70,14 +88,33 @@
 
         public string BuildStadiumPictureUrl(string stadiumName, string imageVersion)
         {
-            string path = "/StadiumPictures/" + string.Format(GlobalConstants.StadiumPicture, stadiumName);
-            var pictureUrl = cloudinary.Api.UrlImgUp.Version(imageVersion).BuildUrl(path);
-            return pictureUrl;
+            if (stadiumName == null || imageVersion == null)
+                return null;
+            
+            string path = string.Format(GlobalConstants.FilePath, StadiumPicturesFolder, string.Format(GlobalConstants.StadiumPicture, stadiumName));
+            return this.BuildPictureUrl(imageVersion, path);
         }
 
         public string BuildPlayerPictureUrl(string playerName, string imageVersion)
         {
-            string path = "/PlayerPictures/" + string.Format(GlobalConstants.PlayerPicture, playerName);
+            if (playerName == null || imageVersion == null)
+                return null;
+            
+            string path = string.Format(GlobalConstants.FilePath, PlayerPicturesFolder, string.Format(GlobalConstants.PlayerPicture, playerName));
+            return this.BuildPictureUrl(imageVersion, path);
+        }
+
+        public string BuildTeamBadgePictureUrl(string teamName, string imageVersion)
+        {
+            if (teamName == null || imageVersion == null)
+                return null;
+
+            string path = string.Format(GlobalConstants.FilePath, TeamBadgesFolder, string.Format(GlobalConstants.BadgePicture, teamName));
+            return this.BuildPictureUrl(imageVersion, path);
+        }
+
+        private string BuildPictureUrl(string imageVersion, string path)
+        {
             var pictureUrl = cloudinary.Api.UrlImgUp.Version(imageVersion).BuildUrl(path);
             return pictureUrl;
         }

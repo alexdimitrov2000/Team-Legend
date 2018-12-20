e024dc9fc613052dba5e55dec952cb1ff6dc2c0b
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2018-12-20 14:49:01 +0200

    Configured ExternalLogins to be set again when there is a login error.

--- TeamLegend/TeamLegend.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ TeamLegend/TeamLegend.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -74,6 +74,7 @@ namespace TeamLegend.Web.Areas.Identity.Pages.Account
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+                ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");

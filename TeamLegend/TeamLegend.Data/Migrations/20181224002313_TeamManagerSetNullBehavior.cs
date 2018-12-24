d560b1023c13540c1c17f389c4c30f99e4d3ceb4
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2018-12-24 02:31:45 +0200

    Edited the delete behavior for Team Manager when Team is deleted.

---
+++ TeamLegend/TeamLegend.Data/Migrations/20181224002313_TeamManagerSetNullBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeamLegend.Data.Migrations
+{
+    public partial class TeamManagerSetNullBehavior : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Managers_Teams_TeamId",
+                table: "Managers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Managers_Teams_TeamId",
+                table: "Managers",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Managers_Teams_TeamId",
+                table: "Managers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Managers_Teams_TeamId",
+                table: "Managers",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}

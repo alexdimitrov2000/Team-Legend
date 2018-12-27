df6c1bea2bfc98639950dc7bf12e6c9a127db9a4
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2018-12-27 22:00:22 +0200

    Exported the piece of code and login from the _ViewStart into a separate UserProfile view component.

---
+++ TeamLegend/TeamLegend.Web/ViewComponents/UserProfileViewComponent.cs
@@ -0,0 +1,33 @@
+﻿namespace TeamLegend.Web.ViewComponents
+{
+    using TeamLegend.Models;
+    using Services.Contracts;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    using System.Threading.Tasks;
+    using System.Security.Claims;
+
+    [ViewComponent(Name = "UserProfile")]
+    public class UserProfileViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly ICloudinaryService cloudinaryService;
+
+        public UserProfileViewComponent(UserManager<ApplicationUser> userManager, ICloudinaryService cloudinaryService)
+        {
+            this.userManager = userManager;
+            this.cloudinaryService = cloudinaryService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(ClaimsPrincipal principalUser)
+        {
+            var user = await this.userManager.GetUserAsync(principalUser);
+
+            var profilePictureUrl = this.cloudinaryService.BuildProfilePictureUrl(user?.UserName, user?.ProfilePictureVersion);
+
+            return this.View((object)profilePictureUrl);
+        }
+    }
+}

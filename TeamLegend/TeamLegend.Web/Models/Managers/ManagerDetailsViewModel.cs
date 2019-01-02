339b8b2825ce61461216a157a8e628e5b08b02c2
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2019-01-02 19:38:41 +0200

    Exported the property display names to private constant strings.

--- TeamLegend/TeamLegend.Web/Models/Managers/ManagerDetailsViewModel.cs
+++ TeamLegend/TeamLegend.Web/Models/Managers/ManagerDetailsViewModel.cs
@@ -7,24 +7,31 @@
 
     public class ManagerDetailsViewModel
     {
+        private const string ManagerNameDisplay = "Name";
+        private const string ManagerAgeDisplay = "Age";
+        private const string ManagerDateOfBirthDisplay = "Date of Birth";
+        private const string ManagerNationalityDisplay = "Nationality";
+        private const string ManagerPictureDisplay = "Manager Picture";
+        private const string ManagerTeamDisplay = "Team";
+
         public string Id { get; set; }
 
-        [Display(Name = "Name")]
+        [Display(Name = ManagerNameDisplay)]
         public string Name { get; set; }
 
-        [Display(Name = "Age")]
+        [Display(Name = ManagerAgeDisplay)]
         public int? Age { get; set; }
 
-        [Display(Name = "Date of Birth")]
+        [Display(Name = ManagerDateOfBirthDisplay)]
         public DateTime? DateOfBirth { get; set; }
 
-        [Display(Name = "Nationality")]
+        [Display(Name = ManagerNationalityDisplay)]
         public string Nationality { get; set; }
 
-        [Display(Name = "Manager Picture")]
+        [Display(Name = ManagerPictureDisplay)]
         public string ManagerPictureUrl { get; set; }
 
-        [Display(Name = "Team")]
+        [Display(Name = ManagerTeamDisplay)]
         public Team Team { get; set; }
     }
 }

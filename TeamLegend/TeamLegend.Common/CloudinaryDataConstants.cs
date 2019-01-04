dc3937ffc91faa442f360f29b4dea04d9eb2104a
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2019-01-04 17:17:33 +0200

    Revealed CloudinaryDataConstants.cs and added it to the project repository.

---
+++ TeamLegend/TeamLegend.Common/CloudinaryDataConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace TeamLegend.Common
+{
+    public static class CloudinaryDataConstants
+    {
+        public static string Cloud = "dux8m4bux";
+
+        public static string ApiKey = "134699845585285";
+
+        public static string ApiSecret = "SxeFab_0_1OA-IJp0yVFKxW8hIw";
+    }
+}
